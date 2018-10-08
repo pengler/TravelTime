7c96b09d931e651023a81f0f7d4b9fc3276141b0
Paul Engler <pengler@gmail.com>
2018-10-08 12:39:31 -0600

    Clean up postal dodes data and add FSAs

--- collectors/convert_canada_add.py
+++ collectors/convert_canada_add.py
@@ -23,18 +23,25 @@ if len (sys.argv) != 3 :
 # 0 indexed vs 1 indexed documentation
 # We are only interester in Civic addresses - (Record_Type_Code == 1)
 titles = ['Record_Type_Code','Address_Type_Code','Province_Code','Directory_Area_Name',
-              'Street_Name','Street_Type_Code','Street_Direction_Code','Street_Address_Sequence_Code',
-              'Street_Address_To_Number','Street_Address_Number_Suffix_To_Code','Suite_To_Number',
-              'Filler1','Street_Address_From_Number','Street_Address_Number_Suffix_From_Code',
-              'Suite_From_Number','Municipality_Name','Filler2','Province_Accent_Indicator',
-              'Directory_Area_Name_Accent_Indicator','Municipality_Name_Accent_Indicator',
-              'Street_Name_Accent_Indicator','Filler3','Postal_Code','Delivery_Installation_Postal_Code',
-              'Action_Code']
-titles = [t.upper() for t in titles]
-numeric_cols = ['Street_Address_To_Number','Street_Address_From_Number','Suite_To_Number','Suite_From_Number']
-numeric_cols = [w.upper() for w in numeric_cols]
+          'Street_Name','Street_Type_Code','Street_Direction_Code','Street_Address_Sequence_Code',
+          'Street_Address_To_Number','Street_Address_Number_Suffix_To_Code','Suite_To_Number',
+          'Filler1','Street_Address_From_Number','Street_Address_Number_Suffix_From_Code',
+          'Suite_From_Number','Municipality_Name','Filler2','Province_Accent_Indicator',
+          'Directory_Area_Name_Accent_Indicator','Municipality_Name_Accent_Indicator',
+          'Street_Name_Accent_Indicator','Filler3','Postal_Code','Delivery_Installation_Postal_Code',
+          'Action_Code']
+to_catagorical = ['Record_Type_Code','Address_Type_Code','Province_Code','Directory_Area_Name',
+          'Street_Name','Street_Type_Code','Street_Direction_Code','Street_Address_Sequence_Code',
+          'Street_Address_Number_Suffix_To_Code', 'Street_Address_Number_Suffix_From_Code',
+          'Municipality_Name', 'Delivery_Installation_Postal_Code','Action_Code']
+to_delete = ['Filler1','Filler2','Province_Accent_Indicator', 'Directory_Area_Name_Accent_Indicator',
+             'Municipality_Name_Accent_Indicator', 'Street_Name_Accent_Indicator','Filler3']          
+to_numeric = ['Street_Address_To_Number','Street_Address_From_Number','Suite_To_Number','Suite_From_Number']
 
-#addresses = pd.DataFrame(columns=add_titles)
+titles = [t.upper() for t in titles]
+to_catagorical = [c.upper() for c in to_catagorical]
+to_delete = [d.upper() for d in to_delete]
+to_numeric = [w.upper() for w in to_numeric]
 
 split_points = [0,1,2,4,34,64,70,72,73,79,80,86,99,105,106,112,142,161,162,163,164,164,167,173,179]
 my_list = []
@@ -46,13 +53,27 @@ with open(sys.argv[1],'r') as file:
                 split_list[x] = split_list[x].strip()
             my_list.append(dict(zip(titles,split_list)))
 
-addresses = pd.DataFrame(my_list)
+postal_code = pd.DataFrame(my_list)
 # for debugging
 #pd.set_option('display.max_columns', 500)
 #pd.set_option('display.width', 1000)
 #print(addresses['Suite_To_Number'])
-#addresses = addresses.rename(str.upper, axis='columns')
-for num in numeric_cols:
-    addresses[num] = pd.to_numeric(addresses[num], downcast='integer', errors='coerce')
 
-pickle.dump( addresses, open( sys.argv[2], "wb" ) )
+postal_code = postal_code.rename(str.upper, axis='columns')
+
+#Clear out columns that won't be used
+postal_code = postal_code.drop(to_delete, axis=1)
+
+#Convert to catagorical
+for col in to_catagorical:                               
+    postal_code[col] = postal_code[col].astype('category')
+
+#Convert to numeric
+for num in to_numeric:
+    postal_code[num] = pd.to_numeric(postal_code[num], downcast='integer', errors='coerce')
+
+#Create a Forward Sortation Area Catagory
+postal_code['FORWARD_SORTATION_AREA'] = postal_code.POSTAL_CODE.str[:3].astype('category')
+
+#Save the dataframe
+pickle.dump( postal_code, open( sys.argv[2], "wb" ) )
--- servers/PostalCode/postal_code.py
+++ servers/PostalCode/postal_code.py
@@ -4,7 +4,7 @@ import flask
 from flask import Flask, jsonify
 import pickle
 import sys
-import pandas
+import pandas as pd
 
 # Create the application.
 APP = flask.Flask(__name__)
@@ -18,12 +18,7 @@ def index():
     """
     return flask.render_template('index.html')
 
-#@APP.route('/hello/<name>/')
-#def hello(name):
-#    """ Displays the page greats who ever comes to visit it.
-#    """
-#    return flask.render_template('hello.html', name=name)
-
+# Return a random postal code
 @APP.route('/api/v0.1/code/', methods=['GET'])
 def random_code():
     random_code = postal_codes.sample().to_json(orient='records')
@@ -34,6 +29,7 @@ def random_code():
     )
     return response
 
+# return a specific postal code
 @APP.route('/api/v0.1/code/<requestcode>/', methods=['GET'])
 def get_code(requestcode):
     lookup_code = postal_codes.loc[postal_codes['POSTAL_CODE'] == requestcode].to_json(orient='records')
@@ -46,7 +42,29 @@ def get_code(requestcode):
     )
     return response
 
+# return a random Forward Sortation Area
+@APP.route('/api/v0.1/fsa/', methods=['GET'])
+def random_fsa():
+    random_fsa =  (postal_codes.sample().FORWARD_SORTATION_AREA).to_frame().to_json(orient='records')
+    response = APP.response_class(
+        response=random_fsa,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
+# return postal codes in a Forward Sortation Area
+@APP.route('/api/v0.1/fsa/<requestfsa>', methods=['GET'])
+def get_fsa(requestfsa):
+    code_list = list(postal_codes.loc[postal_codes['FORWARD_SORTATION_AREA'] == requestfsa ].POSTAL_CODE)
+    code_json = pd.DataFrame({'POSTAL_CODES':[code_list]}).to_json(orient ='records')  
+    #lookup_fsa = postal_codes.loc[postal_codes['FORWARD_SORTATION_AREA'] == requestfsa].to_json(orient='records')
+    response = APP.response_class(
+        response= code_json,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 if __name__ == '__main__':
     APP.debug=True
     APP.run()
